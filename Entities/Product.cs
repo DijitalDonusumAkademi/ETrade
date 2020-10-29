dbbe8cc2ac14babb880aa11af79e070f9ec56e77
Dijital DÖNÜŞÜM <zeki.sert@yahoo.com>
2020-10-29 23:06:49 +0300

    SeedDatabase.cs Eklendi.

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -7,7 +7,7 @@ namespace Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
-
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public List<ProductCategory> ProductCategories { get; set; }
 
