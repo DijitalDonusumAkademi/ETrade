295e790f134a1cda630c93a9f4c16e6535358b76
Dijital DÖNÜŞÜM <zeki.sert@yahoo.com>
2020-10-29 23:27:09 +0300

    ICategoryService.cs Eklendi.

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        Category GetByIdWithProducts(int id);
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+        Category GetById(int id);
+        void DeleteFromCategory(int categoryId, int productId);
+    }
+}
