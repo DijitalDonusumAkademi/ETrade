998e5fe3b59d2561dc593423914660cc535e6cc6
Dijital DÖNÜŞÜM <zeki.sert@yahoo.com>
2020-10-29 23:41:36 +0300

    IOrderService.cs Eklendi.

---
+++ Business/Abstract/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Business.Abstract
+{
+    public interface IOrderService
+    {
+        void Create(Order entity);
+        List<Order> GetOrders(string userId);
+    }
+}
