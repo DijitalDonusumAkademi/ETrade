db12f355ce6749d2e746c07c8c50c27cebfe7e48
Dijital DÖNÜŞÜM <zeki.sert@yahoo.com>
2020-10-29 22:32:46 +0300

    OrderDal.cs Eklendi.

---
+++ DataAccess/Concrete/OrderDal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Abstract;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class OrderDal : Repository<Order,AppDbContext>,IOrderDal
+    {
+        public List<Order> GetOrders(string userId)
+        {
+            using (var context = new AppDbContext())
+            {
+                var orders = context.Orders
+                    .Include(i => i.OrderItems)
+                    .ThenInclude(i => i.Product)
+                    .AsQueryable();
+
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    orders = orders.Where(i => i.UserId == userId);
+                }
+
+                return orders.ToList();
+            }
+        }
+    }
+}
