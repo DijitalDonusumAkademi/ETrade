bc892d7a6afc205e27775eb928d85d23aeab2d5a
Dijital DÖNÜŞÜM <zeki.sert@yahoo.com>
2020-10-29 23:23:04 +0300

    ICartService.cs Eklendi.

---
+++ Business/Abstract/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace Business.Abstract
+{
+    public interface ICartService
+    {
+        void InitializeCart(string userId);
+        Cart GetCartByUserId(string userId);
+
+        void AddToCart(string userId, int productId, int quantity);
+        void DeleteFromCart(string userId, int productId);
+        void ClearCart(object cartId);
+    }
+}
